85c9ceaabef46ae8671a0cd909c24d78c664878a
Vlad-404 <vmijat21@gmail.com>
2021-03-23 15:38:14 +0100

    Ver5: Add recipe list title

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a Test', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_1280.jpg'),
+    new Recipe('A Second Recipe', 'Second recipe description', 'https://pixnio.com/free-images/2021/03/17/2021-03-17-11-55-34-1800x1200.jpg')
+  ];
 
   constructor() { }
 
